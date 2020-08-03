8b35d7509fd6d3422c965f6143bc6a37781f2b15
Elizabeth5 <escheidt5@gmail.com>
2020-08-03 11:15:00 -0400

    test routing deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/my-react-app">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
