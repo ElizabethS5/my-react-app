dad81381593c37f495aeb7a96f5c7eb239c8d2a2
Elizabeth5 <escheidt5@gmail.com>
2020-08-03 10:10:16 -0400

    Updates

---
+++ precache-manifest.86651d0b468037f40b8320d267c7003d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74eb572abd329ace15843b5aa683891a",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "ae4f70ef82a15f5c61b6",
+    "url": "/my-react-app/static/css/main.c1a56feb.chunk.css"
+  },
+  {
+    "revision": "afee6ffc2538f35a554c",
+    "url": "/my-react-app/static/js/2.a0d99481.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-react-app/static/js/2.a0d99481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4f70ef82a15f5c61b6",
+    "url": "/my-react-app/static/js/main.d643e1bc.chunk.js"
+  },
+  {
+    "revision": "3916136fee8b9c9b5407",
+    "url": "/my-react-app/static/js/runtime-main.45ed5c2f.js"
+  }
+]);
