f23f9dbc5b07b15216039eb9214360a1f629286b
Elizabeth5 <escheidt5@gmail.com>
2020-08-03 10:03:24 -0400

    update

---
+++ src/components/page404/Page404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page404(props) {
+  return (
+    <div className="Page404">
+      <h1>404 response - bad request</h1>
+    </div>
+  );
+}
+
+export default Page404;
